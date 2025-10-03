7a4287ae4881e4a1b10cd5b378774a68c313c5e6
BLINK-sys <77540383+BLINK-sys@users.noreply.github.com>
2025-10-03 12:59:54 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
     # Используем переменную окружения для URL базы данных (для Render)
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://pospro:yfcnhjqrf@localhost:5432/pospro_server_db")
+    #SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://pospro:yfcnhjqrf@localhost:5432/pospro_server_db")    
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://pospro_user:FkfSdSLXtK9ZFei3tch3KmUYuLOeq1rO@dpg-d3fnijili9vc73e7pvq0-a/pospro_db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret")
     JWT_IDENTITY_CLAIM = 'sub'
