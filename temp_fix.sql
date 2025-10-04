4f1932ad46be8c459d545b20c58061fb3d74492d
Anton Bocan <bocan.anton@mail.ru>
2025-10-04 16:08:41 +0500

    Cleanup - removed temporary encoding test files

--- temp_fix.sql
+++
@@ -1,5 +0,0 @@
--- Удаляем записи с проблемной кодировкой
-DELETE FROM product_document WHERE filename ~ '[^\x00-\x7F]';
--- Проверяем результат
-SELECT COUNT(*) as total_records FROM product_document;
-SELECT id, filename FROM product_document ORDER BY id;
